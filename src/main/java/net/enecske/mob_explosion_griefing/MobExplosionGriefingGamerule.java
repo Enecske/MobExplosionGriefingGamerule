5953ef94c566ad40a564964dde341c1e993c72a6
Enecske <gergely.duronelly@gmail.com>
2023-10-14 15:55:37 +0200

    1.16.2-1.16.5

--- src/main/java/net/enecske/mob_explosion_griefing/MobExplosionGriefingGamerule.java
+++ src/main/java/net/enecske/mob_explosion_griefing/MobExplosionGriefingGamerule.java
@@ -5,12 +5,12 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MobExplosionGriefingGamerule implements ModInitializer {
 	public static final String MOD_ID = "mob_explosion_griefing";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	public static final GameRules.Key<GameRules.BooleanRule> MOB_EXPLOSION_GRIEFING =
 			GameRuleRegistry.register("mobExplosionGriefing", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
